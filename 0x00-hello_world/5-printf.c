b79d70d467f8b9965dee480d6a4616c9e7c38981
Covenant Osuji <iamcovenant1@gmail.com>
2022-09-08 14:35:25 -0700

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,10 +5,10 @@
  * main - Main printf function
  *
  *
- * Return always 0
+ * Return: always 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, \n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
