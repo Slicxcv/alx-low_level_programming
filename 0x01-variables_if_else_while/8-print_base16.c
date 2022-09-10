530e7722b186d4786a71f191cab6b651844ac99a
Covenant Osuji <iamcovenant1@gmail.com>
2022-09-10 03:46:45 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: prints all the numbers of base 16 in lowercase, \n
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ch;
+	char d = '0';
+
+        while (d <= '9')
+	{
+		putchar(d);
+		d++;
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
