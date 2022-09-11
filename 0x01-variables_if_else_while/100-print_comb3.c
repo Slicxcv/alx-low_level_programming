7f29b80822afdbf3168ce53d1071e891da815a9a
Covenant Osuji <iamcovenant1@gmail.com>
2022-09-11 15:45:02 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,14 +13,14 @@ int main(void)
 	{
 		for (i = '0'; i <= '9'; i++)
 		{
-			if(c < i)
+			if (c < i)
 			{
-														putchar(c);
+				putchar(c);
 				putchar(i);
 
-				if(c != '8' || (c == '8' && i != '9'))
+				if (c != '8' || (c == '8' && i != '9'))
 				{
-															putchar(',');
+					putchar(',');
 					putchar(' ');
 				}
 			}
