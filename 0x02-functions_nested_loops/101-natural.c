ad884e0dd4edcb52abf6c4e486fc75a2ee4ca544
Covenant Osuji <iamcovenant1@gmail.com>
2022-09-13 21:27:39 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 /**
+ * main - prints the sum of all the multiples of 3 or 5
+ * Return: 0
+ */
 int main(void)
 {
 	int n = 0;
@@ -10,7 +13,7 @@ int main(void)
 	{
 		if (i % 3 == 0)
 		n += i;
-		
+
 
 		if (i % 5 == 0)
 		n += i;
