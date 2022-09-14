a7c77d33cb2c822799f9eb3fdf4b80be4640daf8
Covenant Osuji <iamcovenant1@gmail.com>
2022-09-13 20:48:39 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,7 +10,7 @@ void times_table(void)
 	{
 		_putchar('0');
 
-		for (mult = 1: mult <= 9; mult++)
+		for (mult = 1; mult <= 9; mult++)
 		{
 			_putchar(',');
 			_putchar(' ');
@@ -23,6 +23,6 @@ void times_table(void)
 
 			_putchar((prod % 10) + '0');
 		}
-		_putchar('\n')
+		_putchar('\n');
 	}
 }
